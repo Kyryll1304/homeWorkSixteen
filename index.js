898d9f68934587809bc14bbcb25c91e2c523fe3b
Kyryll1304 <kirilltrosnickii@me.com>
2023-01-07 20:36:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function password(length) {
+  let result = "";
+  let characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+console.log(password(16));
